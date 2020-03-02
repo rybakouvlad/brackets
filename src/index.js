4b4572f15792bc69171812baa9cf7fe7f819fe4c
rybakouvlad <rybakou.vladik@gmail.com>
2020-03-03 02:18:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let regexp = new RegExp( bracketsConfig.join('|').replace(/\,/g, "").replace(/([\{\}\(\)\[\]])/g, "\\$1").replace(/(\W+)/g, "$1").replace(/(\|\|)/g, "\\\|\\\|").replace(/(\\\|\\\|\|)/g, "|\\\|\\\|"));
+
+    for (let i = 0; i < str.length; i++) {
+      str = str.replace(regexp, '');
+      if (str != str.replace(regexp, '')) {
+        i = i - 2;
+      }
+    }
+  
+    if (str.length == 0) {
+  
+      return true;
+    }return false;  
+  
 }
